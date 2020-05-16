95de760efb34939a1e30a0d9158b1bfa7182de2e
unknown <mamamiahudai@gmail.com>
2020-05-16 22:38:52 +0800

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,181 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                @if($n)
+                <div class="card-header text-center">Nice to meet with you  <span style="font-size: 14px;">{{$n}}</span> !</div>
+  @endif
+                <div class="card-body bg-dark">
+                 <div class="col-md-4">
+                    
+      <img  style="width: 285px;" src="{{asset('img/pro.jpg')}}">
+   
+  </div>
+  <div class="col-md-8">
+    <div class="panel panel-default">
+  <div class="panel-body text-center">
+      
+<h3>Khalid Rahman</h3>
+<p>Web Developer</p>
+<p>fube.khalid@gmail.com</p>
+<p>+60172451604</p>
+<button class="btn btn-success"><i class="fa fa-download"></i> Download Resume</button>
+
+
+
+
+  </div>
+</div>
+        
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="{{asset('img/pro.jpg')}}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="{{asset('img/pro.jpg')}}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+  <div class="col-md-4">
+     <div class="panel panel-default">
+  <div class="panel-body text-center">
+    <div class="card" style="width: 100%;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+</div>
+
+      
+  </div>
+</div>
+                     
+                 </div>
+
+                    
+                    <div style="margin-top: 100%;"></div>
+                </div>
+              
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,27 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Khalid Rahman</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('/css/font-awesome/css/font-awesome.min.css')}}">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-image: url('/img/bg3.png');
+
+                color: #fff;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
+
             }
 
             .full-height {
@@ -42,10 +49,17 @@
 
             .content {
                 text-align: center;
+                border:none;
+                border-radius: 10px;
+                  background: rgba(0, 0, 0, 0.85);
+                  height: 60%;
+                  margin: 10%;
+                  padding: 1%;
+                  
             }
 
             .title {
-                font-size: 84px;
+                font-size: 60px;
             }
 
             .links > a {
@@ -60,6 +74,17 @@
 
             .m-b-md {
                 margin-bottom: 30px;
+            }
+
+            .btn-lets {
+                background-color: #4a1679;
+                color: white;
+                font-weight: 600;
+                font-size: 14px;
+                margin-top: 5%;
+            }
+            .gap2x {
+                margin-top: 20%;
             }
         </style>
     </head>
@@ -78,22 +103,34 @@
                     @endauth
                 </div>
             @endif
-
+<div class="container">
+    <div class="row">
+        <div class="col">
+            
+     
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
+                    <div class="form-group">
+                        <form action="name" method="get">
+                             {{ csrf_field() }}
+                        This is khalid here<br>
+  <label for="usr"> And You ?</label>
+  <input type="text" class="form-control " id="usr" name="N" placeholder="Type Your Name Here">
+  <button  class="btn btn btn-lets btn-lg text-dark " style="margin-bottom: 5%;" >Lets go !</button>
+</form>
+ 
+</div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <p>Don't Worry I am not storing your name on Database</p>
+
             </div>
+
+               </div>
+        
+    </div>
+    
+</div>
         </div>
     </body>
 </html>
